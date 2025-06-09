6e6327c6a9f2ba3efab7b00a316668c5bcb60409
arun <global@gmail.com>
2025-06-09 17:02:00 +0530

    Initial push

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+from langchain_groq import ChatGroq
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+GROQ_API_KEY=os.environ.get('GROQ_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+
+index_name = "medicalbottt"
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+
+# Initialize Groq LLM (LLaMA3-70B)
+llm = ChatGroq(
+    api_key=GROQ_API_KEY,
+    model_name="llama3-70b-8192",  # Groq supports this model
+    temperature=0.4,
+    max_tokens=500,
+)
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+# Create chat prompt template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    ("human", "{input}"),
+])
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+@app.route("/")
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return f"Template Rendering failed: {str(e)}"
+
+
+@app.route("/get", methods=["GET", "POST"])
+
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Generative-AI-Chatbot",
+    version = '0.0.0',
+    author = "Lucifer",
+    author_email = "luxferro73@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
